5e1615f585e76d77c32349d25b2fdc68535e9b9b
Henrique Rodrigues <henrique.grdr@inf.ufpel.edu.br>
2024-12-14 18:01:52 -0300

    upload: project upload

---
+++ script.js
@@ -0,0 +1,60 @@
+// ==UserScript==
+// @name         e-Aula Dark Mode
+// @namespace    https://github.com/Equiel-1703
+// @version      1.0
+// @description  Habilita dark mode na plataforma e-aula da UFPel.
+// @author       Henrique Rodrigues Barraz
+// @license      MIT
+// @match        https://e-aula.ufpel.edu.br/*
+// @icon         https://lh3.google.com/u/2/d/1w_3Rk94K25OsIEpq0M9muoFHrxjOIuuK
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    const new_css = `
+:root {
+	--bg-color: black;
+	--cards-bg-color: #333;
+	--text-color: white;
+	--titles-color: antiquewhite;
+}
+
+body {
+	background-color: var(--bg-color);
+	color: var(--text-color);
+}
+
+body #page div#topofscroll.main-inner {
+	background-color: var(--bg-color);
+	color: var(--text-color);
+}
+
+body #page h1,
+body #page h2,
+body #page h3,
+body #page h4,
+body #page h5,
+body #page h6 {
+	color: var(--titles-color);
+}
+
+body#page-my-index #page #topofscroll div.card-body,
+body#page-my-index #page #topofscroll .card,
+body#page-site-index #page #page-content #region-main {
+	background-color: var(--cards-bg-color);
+	color: var(--text-color);
+}
+	`;
+
+	const append_css = (css) => {
+		const style_tag = document.createElement('style');
+
+		style_tag.innerHTML = css;
+
+		document.head.appendChild(style_tag);
+	};
+
+	append_css(new_css);
+})();
